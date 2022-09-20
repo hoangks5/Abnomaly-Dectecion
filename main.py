e7582b9c1f277ee9b28b8d33a4b47fa4ec984e8f
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 17:53:41 +0700

    --- hoangks5 ---

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from adtk.data import validate_series
 import pandas as pd
 
 
-s_train = pd.read_csv("./BTC_USD.csv", index_col="Ngày", parse_dates=True, squeeze=True)
+s_train = pd.read_csv("./BTC_USD.csv", index_col="Date", parse_dates=True, squeeze=True)
 
 s_train = validate_series(s_train)
 print(s_train)
