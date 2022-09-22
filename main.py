d95215336384b1b1e01ef85c34f1ab302ecbbfc9
hoangks5 <hoangnd.ute@gmail.com>
2022-09-22 13:50:00 +0700

    --- hoangks5 ---

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import pandas as pd
 s = pd.read_csv('./BTC_USD.csv', index_col="Date", parse_dates=True, squeeze=True)
 from adtk.data import validate_series
 s = validate_series(s)
+
+from adtk.detector import ThresholdAD
+threshold_ad = ThresholdAD(high=50000, low=30000)
+anomalies = threshold_ad.detect(s)
