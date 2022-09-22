4cb55bb4a6f28be7b6df6c1f76b18c8c9aaa5ec7
hoangks5 <hoangnd.ute@gmail.com>
2022-09-22 13:54:11 +0700

    --- hoangks5 ---

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ threshold_ad = ThresholdAD(high=50000, low=30000)
 anomalies = threshold_ad.detect(s)
 from adtk.visualization import plot
 t = plot(s, anomaly=anomalies, ts_linewidth=1, ts_markersize=3, anomaly_markersize=5, anomaly_color='red', anomaly_tag="marker")
-print(t)
+t.figure
