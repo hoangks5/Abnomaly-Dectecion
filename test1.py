d3a689019b04738f525f60309f5e216d7ccb1d49
hoangks5 <hoangnd.ute@gmail.com>
2022-09-26 17:26:25 +0700

    --- hoangks5 ---

--- test1.py
+++ test1.py
@@ -18,13 +18,7 @@ def upload(file: UploadFile = File(...)):
        
         y.append(line.split(',')[0])
         plt.plot(x,)
-    x1 = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in x]
-
-    import matplotlib.dates as mdates
-
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    plt.plot(x1,y)
-    plt.gcf().autofmt_xdate()
+    
+    plt.plot(x,len(x))
     plt.show()
   
