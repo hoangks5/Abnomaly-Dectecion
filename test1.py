30a8b8bba6f62db56ddeb6527cd52a09d37ec54d
hoangks5 <hoangnd.ute@gmail.com>
2022-09-26 17:41:05 +0700

    --- hoangks5 ---

--- test1.py
+++ test1.py
@@ -13,7 +13,10 @@ async def post_endpoint(in_file: UploadFile=File(...)):
     async with aiofiles.open('ok.csv', 'wb') as out_file:
         content = await in_file.read()  # async read
         await out_file.write(content)  # async write
-
+    import pandas as pd
+    s = pd.read_csv('./BTC_USD.csv', index_col="Date", parse_dates=True, squeeze=True)
+    from adtk.data import validate_series
+    s = validate_series(s)
     return {"Result": "OK"}
     
   
