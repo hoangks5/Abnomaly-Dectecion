52f0eb452cc6503b12a530c685715ffe8a0f0bd5
hoangks5 <hoangnd.ute@gmail.com>
2022-09-26 16:55:34 +0700

    --- hoangks5 ---

---
+++ test1.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+import matplotlib.pyplot as plt
+app = FastAPI()
+
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    contents = file.file.read()
+
+    contents.decode("utf-8") 
+    return contents
+  
