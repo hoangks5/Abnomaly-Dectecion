6cf13ab579239464f93355daa0ac08fa7cd55524
hoangks5 <hoangnd.ute@gmail.com>
2022-09-26 17:38:55 +0700

    --- hoangks5 ---

--- test1.py
+++ test1.py
@@ -10,7 +10,7 @@ app = FastAPI()
 @app.post("/")
 async def post_endpoint(in_file: UploadFile=File(...)):
     # ...
-    async with aiofiles.open(in_file.filename, 'wb',encoding='utf-8') as out_file:
+    async with aiofiles.open('ok.csv', 'wb',encoding='utf-8') as out_file:
         content = await in_file.read()  # async read
         await out_file.write(content)  # async write
 
