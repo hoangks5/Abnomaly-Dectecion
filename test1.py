11a3ba76aabd423f327db6be760af873c42a2043
hoangks5 <hoangnd.ute@gmail.com>
2022-09-26 17:06:14 +0700

    --- hoangks5 ---

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.post("/upload")
 def upload(file: UploadFile = File(...)):
     contents = file.file.read()
-    t = contents.decode("utf-8")
+    contents = contents.decode("utf-8")
     print(type(t))
     return 0
   
