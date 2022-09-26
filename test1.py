fc02dd99dd138bac880eb5c9a7361090d7bf4a95
hoangks5 <hoangnd.ute@gmail.com>
2022-09-26 17:15:05 +0700

    --- hoangks5 ---

--- test1.py
+++ test1.py
@@ -8,10 +8,12 @@ app = FastAPI()
 @app.post("/upload")
 def upload(file: UploadFile = File(...)):
     x = []
+    y = []
     contents = file.file.read()
     contents = contents.decode("utf-8").splitlines()
     
     for line in contents[1:]:
         x.append(line.split(',')[1])
+        
     return x
   
